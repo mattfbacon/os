c298e1eac71bbfdacb481fa222b6b3d6120d939f
Matt Fellenz <mattf53190@gmail.com>
2021-03-26 07:45:57 -0700

    Some revisions to the task info stuff
    - Remove tasks.asm because it does not really match the multitasking
    system, and also I want to do it in C.
    - Remove tcb.inc because it's unnecessary
    - Remove `K_RSP` from the TCB structure, since it has no purpose. I'm
    honestly not sure what it's supposed to do; the interrupts have their
    own stack pointer, and the stack pointer for kernel_main is stored in
    the TSS.
    
    In the future it will be necessary to add a nested struct to the TCB
    struct to save the registers.

--- src/intf/tcb.h
+++ src/intf/tcb.h
@@ -1,12 +1,10 @@
 struct __attribute__((packed)) tcb {
-	struct tcb* next; // next task pointer (for scheduling)
+	struct tcb* next; // next task pointer (for scheduling; forms a circular linked list)
 	uint64_t t_rsp; // stack pointer for the thread
-	uint64_t t_cr3; // paging control for the thread
-	uint64_t k_rsp; // kernel stack pointer for this thread
+	uint64_t t_cr3; // page dir for the thread
 
 	uint8_t state; // task state bitfield: <structure TBD>
 	uint8_t priority; // task priority: higher is more important
-	uint16_t reserved_0;
-	uint16_t owner_id; // owner process ID
-	uint16_t task_id; // task ID under process
+	uint8_t reserved_0;
+	uint8_t owner_id; // owner process ID
 };
