180bef8f6638f907562f5898ea68c513b2dbc1fd
Matt Fellenz <mattf53190@gmail.com>
2021-03-30 21:20:30 -0700

    video memory should be volatile

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -13,7 +13,7 @@ struct Char {
 	uint8_t color;
 };
 
-struct Char* buffer = (struct Char*)0xb8000;
+volatile struct Char* buffer = (struct Char*)0xb8000;
 size_t col = 0;
 size_t row = 0;
 uint8_t color = PRINT_COLOR_WHITE | (PRINT_COLOR_BLACK << 4);
